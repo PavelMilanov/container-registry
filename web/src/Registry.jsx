00ac39a8157a5035632f7a993bf9a460b80e5fe7
pavel_milanov <milanov71@icloud.com>
2025-02-20 21:19:18 +0300

    рефакторинг

--- web/src/Registry.jsx
+++ web/src/Registry.jsx
@@ -1,6 +1,6 @@
 import { createSignal, onMount, lazy } from "solid-js"
 import { A, useNavigate } from "@solidjs/router"
-import axios from 'axios'
+import axios from "axios"
 import toast from "solid-toast"
 
 const AddRegistry = lazy(() => import("./modal/AddRegistry"))
@@ -9,148 +9,167 @@ const Delete = lazy(() => import("./modal/Delete"))
 const API_URL = window.API_URL
 
 function Registry() {
-    const navigate = useNavigate()
-    const [isModalOpen, setModalOpen] = createSignal(false)
-    const [registryList, setRegistryList] = createSignal([])
-    const [registry, setRegistry] = createSignal('')
-    
-    const openModal = () => setModalOpen(true)
-    const closeModal = () => setModalOpen(false)
-    const submitAddRegistry = async () => {
-        setModalOpen(false)
-        let token = localStorage.getItem('token')
-        const headers = {
-            'Authorization': `Bearer ${token}`
-        }
-        try {
-            const response = await axios.post(
-                API_URL + `/api/registry/${registry()}`,
-                {},
-                { headers }
-            );
-            if (response.status === 201) {
-                await getRegistry()
-                toast("Реестр добавлен!", {
-                    style: {
-                        "background-color": "#1e3c72",
-                        "color": "white"
-                    },
-                    className: "notification",
-                })
-            }
-        } catch (error) {
-            console.log(error.response.data.error)
-            if (error.response.status === 401) {
-                localStorage.removeItem("token")
-                navigate("/login", { replace: true })
-            }
-        }
+  const navigate = useNavigate()
+  const [isModalOpen, setModalOpen] = createSignal(false)
+  const [registryList, setRegistryList] = createSignal([])
+  const [registry, setRegistry] = createSignal("")
+
+  const openModal = () => setModalOpen(true)
+  const closeModal = () => setModalOpen(false)
+  const submitAddRegistry = async () => {
+    setModalOpen(false)
+    let token = localStorage.getItem("token")
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    try {
+      const response = await axios.post(
+        API_URL + `/api/registry/${registry()}`,
+        {},
+        { headers },
+      );
+      if (response.status === 201) {
+        await getRegistry();
+        toast("Реестр добавлен!", {
+          style: {
+            "background-color": "#1e3c72",
+            color: "white",
+          },
+          className: "notification",
+        });
+      }
+    } catch (error) {
+      console.log(error.response.data.error)
+      if (error.response.status === 401) {
+        localStorage.removeItem("token")
+        navigate("/login", { replace: true })
+      }
     }
-    const newRegistry = (value) => setRegistry(value)
+  };
+  const newRegistry = (value) => setRegistry(value);
 
-    const [isModalDeleteOpen, setModalDeleteOpen] = createSignal(false)
+  const [isModalDeleteOpen, setModalDeleteOpen] = createSignal(false)
 
-    let copyText = `${API_URL}`.split("//")[1]
+  let copyText = `${API_URL}`.split("//")[1]
 
-    const openDeleteModal = (item) => {
-        setModalDeleteOpen(true)
-        setRegistry(item)
+  const openDeleteModal = (item) => {
+    setModalDeleteOpen(true)
+    setRegistry(item)
+  };
+  const closeDeleteModal = () => setModalDeleteOpen(false)
+  const submitDelete = async () => {
+    setModalDeleteOpen(false)
+    let token = localStorage.getItem("token")
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    try {
+      const response = await axios.delete(
+        API_URL + `/api/registry/${registry()}`,
+        { headers: headers },
+      );
+      if (response.status == 202) {
+        toast("Реестр удален!", {
+          style: {
+            "background-color": "#1e3c72",
+            color: "white",
+          },
+          className: "notification",
+        });
+      }
+      await getRegistry();
+    } catch (error) {
+      console.error(error.response.data);
+      if (error.response.status === 401) {
+        localStorage.removeItem("token")
+        navigate("/login", { replace: true })
+      }
     }
-    const closeDeleteModal = () => setModalDeleteOpen(false)
-    const submitDelete = async () => {
-        setModalDeleteOpen(false)
-        let token = localStorage.getItem('token')
-        const headers = {
-            'Authorization': `Bearer ${token}`
-        }
-        try {
-            const response = await axios.delete(
-                API_URL + `/api/registry/${registry()}`,
-                { headers: headers }
-            )
-            if (response.status == 202) {
-                toast("Реестр Удален!", {
-                    style: {
-                        "background-color": "#1e3c72",
-                        "color": "white"
-                    },
-                    className: "notification",
-                })
-            }
-            await getRegistry()
-        } catch (error) {
-            console.error(error.response.data)
-            if (error.response.status === 401) {
-                localStorage.removeItem("token")
-                navigate("/login", {replace: true})
-            }
-        }
+  };
+
+  async function getRegistry() {
+    let token = localStorage.getItem("token")
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    try {
+      const response = await axios.get(API_URL + "/api/registry", {
+        headers: headers,
+      });
+      setRegistryList(response.data.data) // в ответе приходит массив "data"
+    } catch (error) {
+      console.error(error.response.data)
+      if (error.response.status === 401) {
+        localStorage.removeItem("token")
+        navigate("/login", { replace: true })
+      }
     }
+  }
 
-    async function getRegistry() {
-        let token = localStorage.getItem('token')
-        const headers = {
-            'Authorization': `Bearer ${token}`
-        }
-        try {
-            const response = await axios.get(API_URL + "/api/registry", {headers: headers})
-            setRegistryList(response.data.data)// в ответе приходит массив "data"
-        } catch (error) {
-            console.error(error.response.data)
-            if (error.response.status === 401) {
-                localStorage.removeItem("token")
-                navigate("/login", {replace: true})
-            }
-        }
-    }
+  onMount(async () => {
+    await getRegistry()
+  });
 
-    onMount(async () => {
-        await getRegistry()
-    })
-
-    return (
-        <div class="container">
-            <h2>Репозитории</h2>
-            <div class="copy-container">
-                <input
-                    type="text"
-                    value={copyText}
-                    readonly
-                />
-            </div>
-            <div class="card">
-                <button class="btn btn-primary" onClick={openModal}>Добавить реестр</button>
-                <AddRegistry isOpen={isModalOpen()} onNewRegistry={newRegistry} onClose={closeModal} onSubmit={submitAddRegistry} />
-                <Delete isOpen={isModalDeleteOpen()} message={"Все репозитории и образы Docker реестра будут удалены!"} onClose={closeDeleteModal} onSubmit={submitDelete} />
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Реестр</th>
-                            {/* <th>Размер</th> */}
-                            <th>Создан</th>
-                            <th>...</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <For each={registryList()} >{(registy, i) =>
-                            <tr>
-                                <td>
-                                    <div>
-                                        <A inactiveClass="" href={registy.Name}>{registy.Name}</A>
-                                    </div>
-                                </td>
-                                {/* <td>{registy.Size}</td> */}
-                                <td>{registy.CreatedAt}</td>
-                                <td>
-                                    <button class="btn btn-secondary" onClick={() => openDeleteModal(registy.Name)}>Удалить реестр</button>
-                                </td>
-                            </tr>
-                        }</For>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
+  return (
+    <div class="container">
+      <h2>Репозитории</h2>
+      <div class="copy-container">
+        <input type="text" value={copyText} readonly />
+      </div>
+      <div class="card">
+        <button class="btn btn-primary" onClick={openModal}>
+          Добавить реестр
+        </button>
+        <AddRegistry
+          isOpen={isModalOpen()}
+          onNewRegistry={newRegistry}
+          onClose={closeModal}
+          onSubmit={submitAddRegistry}
+        />
+        <Delete
+          isOpen={isModalDeleteOpen()}
+          message={"Все репозитории и образы Docker реестра будут удалены!"}
+          onClose={closeDeleteModal}
+          onSubmit={submitDelete}
+        />
+        <table>
+          <thead>
+            <tr>
+              <th>Реестр</th>
+              {/* <th>Размер</th> */}
+              <th>Создан</th>
+              <th>...</th>
+            </tr>
+          </thead>
+          <tbody>
+            <For each={registryList()}>
+              {(registy, i) => (
+                <tr>
+                  <td>
+                    <div>
+                      <A inactiveClass="" href={registy.Name}>
+                        {registy.Name}
+                      </A>
+                    </div>
+                  </td>
+                  {/* <td>{registy.Size}</td> */}
+                  <td>{registy.CreatedAt}</td>
+                  <td>
+                    <button
+                      class="btn btn-secondary"
+                      onClick={() => openDeleteModal(registy.Name)}
+                    >
+                      Удалить реестр
+                    </button>
+                  </td>
+                </tr>
+              )}
+            </For>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Registry
--- web/src/Repo.jsx
+++ web/src/Repo.jsx
@@ -7,94 +7,118 @@ const Delete = lazy(() => import("./modal/Delete"))
 const API_URL = window.API_URL
 
 function Repo() {
-    const navigate = useNavigate()
-    const [imageList, setImageList] = createSignal([])
-    const params = useParams()
-    const [repo, setRepo] = createSignal('')
-    const [isModalDeleteOpen, setModalDeleteOpen] = createSignal(false)
-    const openDeleteModal = (item) => {
-        setModalDeleteOpen(true)
-        setRepo(item)
-    }
-    const closeDeleteModal = () => setModalDeleteOpen(false)
+  const navigate = useNavigate()
+  const [imageList, setImageList] = createSignal([])
+  const params = useParams()
+  const [repo, setRepo] = createSignal("")
+  const [isModalDeleteOpen, setModalDeleteOpen] = createSignal(false)
+  const openDeleteModal = (item) => {
+    setModalDeleteOpen(true)
+    setRepo(item)
+  };
+  const closeDeleteModal = () => setModalDeleteOpen(false)
 
-    const submitDelete = async () => {
-        setModalDeleteOpen(false)
-        let token = localStorage.getItem('token')
-        const headers = {
-            'Authorization': `Bearer ${token}`
-        }
-        try {
-            const response = await axios.delete(
-                API_URL + `/api/registry/${params.name}/${repo()}`,
-                { headers: headers }
-            )
-            console.log(response.status)
-            await getRepo()
-        } catch (error) {
-            console.error(error.response.data)
-            if (error.response.status === 401) {
-                localStorage.removeItem("token")
-                navigate("/login", { replace: true })
-            }
-        }
+  const submitDelete = async () => {
+    setModalDeleteOpen(false)
+    let token = localStorage.getItem("token")
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    try {
+      const response = await axios.delete(
+        API_URL + `/api/registry/${params.name}/${repo()}`,
+        { headers: headers },
+      );
+      if (response.status == 202) {
+        toast("Репозиторий удален!", {
+          style: {
+            "background-color": "#1e3c72",
+            color: "white",
+          },
+          className: "notification",
+        });
+      }
+      await getRepo();
+    } catch (error) {
+      console.error(error.response.data)
+      if (error.response.status === 401) {
+        localStorage.removeItem("token")
+        navigate("/login", { replace: true })
+      }
     }
+  };
 
-    async function getRepo() {
-        let token = localStorage.getItem('token')
-        const headers = {
-            'Authorization': `Bearer ${token}`
-        }
-        try {
-            const response = await axios.get(
-                API_URL + `/api/registry/${params.name}`,
-                { headers: headers }
-            )
-            setImageList(response.data.data)  // в ответе приходит массив "data"
-        } catch (error) {
-            console.log(error.response.data)
-            if (error.response.status === 401) {
-                localStorage.removeItem("token")
-                navigate("/login", { replace: true })
-            }
-        }
+  async function getRepo() {
+    let token = localStorage.getItem("token")
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    try {
+      const response = await axios.get(
+        API_URL + `/api/registry/${params.name}`,
+        { headers: headers },
+      );
+      setImageList(response.data.data); // в ответе приходит массив "data"
+    } catch (error) {
+      console.log(error.response.data);
+      if (error.response.status === 401) {
+        localStorage.removeItem("token")
+        navigate("/login", { replace: true })
+      }
     }
+  }
 
-    onMount(async () => {
-        await getRepo()
-    })
-    return (
-        <div class="container">
-            <h2><a href="/registry">Репозитории</a> {'/'} {params.name}</h2>
-            <div class="card">
-                <Delete isOpen={isModalDeleteOpen()} message={"Образы Docker репозитория будут удалены!"} onClose={closeDeleteModal} onSubmit={submitDelete} />
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Репозиторий</th>
-                            {/* <th>Размер</th> */}
-                            <th>Создан</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <For each={imageList()} >{(image, i) =>
-                            <tr>
-                                <td>
-                                    <A inactiveClass="" href={image.Name}>{API_URL.split("//")[1]}/{image.Name}</A>
-                                </td>
-                                {/* <td>{repo.Size}</td> */}
-                                <td>{image.CreatedAt}</td>
-                                <td>
-                                    <button class="btn btn-secondary" onClick={() => openDeleteModal(image.Name)}>Удалить репозиторий</button>
-                                </td>
-                            </tr>
-                        }</For>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
+  onMount(async () => {
+    await getRepo()
+  });
+  return (
+    <div class="container">
+      <h2>
+        <a href="/registry">Репозитории</a> {"/"} {params.name}
+      </h2>
+      <div class="card">
+        <Delete
+          isOpen={isModalDeleteOpen()}
+          message={"Образы Docker репозитория будут удалены!"}
+          onClose={closeDeleteModal}
+          onSubmit={submitDelete}
+        />
+        <table>
+          <thead>
+            <tr>
+              <th>Репозиторий</th>
+              {/* <th>Размер</th> */}
+              <th>Создан</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <For each={imageList()}>
+              {(image, i) => (
+                <tr>
+                  <td>
+                    <A inactiveClass="" href={image.Name}>
+                      {API_URL.split("//")[1]}/{image.Name}
+                    </A>
+                  </td>
+                  {/* <td>{repo.Size}</td> */}
+                  <td>{image.CreatedAt}</td>
+                  <td>
+                    <button
+                      class="btn btn-secondary"
+                      onClick={() => openDeleteModal(image.Name)}
+                    >
+                      Удалить репозиторий
+                    </button>
+                  </td>
+                </tr>
+              )}
+            </For>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Repo
