a93d7f7da165b170bee1160cff1a362d354697c4
Pavel Milanov <milanov71@icloud.com>
2025-03-04 14:01:23 +0300

    тесты API

--- src/main_test.go
+++ src/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -19,7 +20,7 @@ var s = storage.NewStorage(env)
 var sqlite = db.NewDatabase("test.db")
 var h = handlers.NewHandler(s, &sqlite, env)
 
-func TestRegistration(t *testing.T) {
+func TestRegistrationAPI(t *testing.T) {
 	srv := h.InitRouters()
 	token := ""
 	t.Run("registration", func(t *testing.T) {
@@ -36,6 +37,7 @@ func TestRegistration(t *testing.T) {
 		body := `{"username": "test","password":"test"}`
 		req, _ := http.NewRequest("POST", "/login", strings.NewReader(body))
 		srv.ServeHTTP(w, req)
+
 		var response map[string]any
 		err := json.Unmarshal(w.Body.Bytes(), &response)
 		if err != nil {
@@ -46,6 +48,98 @@ func TestRegistration(t *testing.T) {
 		}
 		token = response["token"].(string)
 		t.Log(token)
+	})
+	os.Remove("test.db")
+}
+
+func TestRegistyAPI(t *testing.T) {
+	srv := h.InitRouters()
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDExNjA4NTUsImlhdCI6MTc0MTA3NDQ1NX0.IolFwVdG4GxiSMqlqlpD3YQlxmKFSlkipFZsh3GJMmM"
+	t.Run("add registry", func(t *testing.T) {
+		t.Run("with authorization", func(t *testing.T) {
+			t.Parallel()
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("POST", "/api/registry/test", nil)
+			jwt := fmt.Sprintf("Bearer %s", token)
+			req.Header.Set("Authorization", jwt)
+			srv.ServeHTTP(w, req)
+
+			var response db.Registry
+			err := json.Unmarshal(w.Body.Bytes(), &response)
+			if err != nil {
+				t.Error(err)
+			}
+			if w.Code != http.StatusCreated {
+				t.Errorf("Ошибка при создании репозитория: %s", w.Body.Bytes())
+			}
+			t.Logf("%+v", response)
+		})
+		t.Run("without authorization", func(t *testing.T) {
+			t.Parallel()
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("POST", "/api/registry/test", nil)
+			srv.ServeHTTP(w, req)
+
+			if w.Code != http.StatusUnauthorized {
+				t.Error("Неавторизованный запрос")
+			}
+		})
+	})
+	t.Run("get registry", func(t *testing.T) {
+		t.Run("with authorization", func(t *testing.T) {
+			t.Parallel()
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", "/api/registry/test", nil)
+			jwt := fmt.Sprintf("Bearer %s", token)
+			req.Header.Set("Authorization", jwt)
+			srv.ServeHTTP(w, req)
+
+			response := string(w.Body.Bytes())
+			if w.Code != http.StatusOK {
+				t.Errorf("Ошибка при получения репозиториев: %s", response)
+			}
+			t.Logf("%s", response)
+		})
+		t.Run("without authorization", func(t *testing.T) {
+			t.Parallel()
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", "/api/registry/test", nil)
+			srv.ServeHTTP(w, req)
+
+			if w.Code != http.StatusUnauthorized {
+				t.Error("Неавторизованный запрос")
+			}
+		})
+	})
+	t.Run("delete registry", func(t *testing.T) {
+		t.Run("with authorization", func(t *testing.T) {
+			t.Parallel()
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("DELETE", "/api/registry/test", nil)
+			jwt := fmt.Sprintf("Bearer %s", token)
+			req.Header.Set("Authorization", jwt)
+			srv.ServeHTTP(w, req)
+
+			var response db.Registry
+			err := json.Unmarshal(w.Body.Bytes(), &response)
+			if err != nil {
+				t.Error(err)
+			}
+			if w.Code != http.StatusAccepted {
+				t.Errorf("Ошибка при удалении репозитория: %+v", response)
+			}
+			t.Logf("%+v", response)
+		})
+		t.Run("without authorization", func(t *testing.T) {
+			t.Parallel()
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("DELETE", "/api/registry/test", nil)
+			srv.ServeHTTP(w, req)
+
+			if w.Code != http.StatusUnauthorized {
+				t.Error("Неавторизованный запрос")
+			}
+		})
 	})
 	os.Remove("test.db")
 }
