3e566a1cdc821ee30aafac62788ef8d6e48f4df8
pavel_milanov <milanov71@icloud.com>
2025-04-01 23:13:11 +0300

    переписан компонент выхода

--- web/src/modal/Logout.jsx
+++ web/src/modal/Logout.jsx
@@ -1,10 +1,13 @@
-import { createSignal } from "solid-js";
+import { createSignal, Show } from "solid-js";
+import { Portal } from "solid-js/web";
 import { useNavigate } from "@solidjs/router";
 
 export default function Logout() {
-  const [isModalOpen, setIsModalOpen] = createSignal(false);
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const [showModal, setShowModal] = createSignal(false);
+  const closeModal = () => {
+    navigate(-1);
+    setShowModal(false);
+  };
   const navigate = useNavigate();
 
   const logout = () => {
@@ -14,29 +17,8 @@ export default function Logout() {
   };
 
   return (
-    <div class="relative p-4 w-full max-w-md max-h-full">
-      <div class="relative bg-white rounded-lg shadow-sm">
-        <button
-          type="button"
-          class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-        >
-          <svg
-            class="w-3 h-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-            />
-          </svg>
-          <span class="sr-only">Close modal</span>
-        </button>
+    <div class="p-4 w-full fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-gray-900 modal">
+      <div class="bg-white rounded-lg shadow-sm">
         <div class="p-4 md:p-5 text-center">
           <svg
             class="mx-auto mb-4 text-gray-400 w-12 h-12"
@@ -54,21 +36,21 @@ export default function Logout() {
             />
           </svg>
           <h3 class="mb-5 text-lg font-normal text-gray-500">
-            Are you sure you want to delete this product?
+            Вы действительно хотите выйти?
           </h3>
           <button
-            data-modal-hide="popup-modal"
             type="button"
+            onClick={logout}
             class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
           >
-            Yes, I'm sure
+            Выйти
           </button>
           <button
-            data-modal-hide="popup-modal"
             type="button"
+            onClick={closeModal}
             class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
           >
-            No, cancel
+            Отмена
           </button>
         </div>
       </div>
