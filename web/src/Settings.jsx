e098696f20d2e1a86ddbbf7955f2d27c3902bccb
pavel_milanov <milanov71@icloud.com>
2025-04-02 21:29:53 +0300

    рефакторинг

--- web/src/Settings.jsx
+++ web/src/Settings.jsx
@@ -90,19 +90,7 @@ export default function Settings() {
         <h2>Общие настройки</h2>
         <div class="card">
           <div class="relative w-1/4 m-2">
-            <input
-              type="text"
-              id="disabled_filled"
-              class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=""
-              disabled
-            />
-            <label
-              for="disabled_filled"
-              class="absolute text-sm text-gray-400 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
-            >
-              Удалить неиспользуемые файлы реестра:
-            </label>
+            <p>Удалить неиспользуемые файлы реестра:</p>
           </div>
           <button
             class="relative text-white bg-main border hover:text-main hover:bg-white hover:border focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-2"
@@ -133,19 +121,7 @@ export default function Settings() {
             Сохранить настройки
           </button>
           <div class="relative w-1/8 m-2 mt-4">
-            <input
-              type="text"
-              id="disabled_outlined"
-              class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              disabled
-            />
-            <label
-              for="disabled_outlined"
-              class="absolute text-sm text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
-            >
-              {version()}
-            </label>
+            <p>Версия: {version}</p>
           </div>
         </div>
       </div>
